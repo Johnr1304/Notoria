861056a0ede993b0b2c7c05aa28e7fcc8d389776
Johnr1304 <brittojohn@gmail.com>
2026-09-24 13:19:43 +0530

    Connect frontend to deployed backend

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://notoria-backend-6qj0.onrender.com/api",
 });
 
 export default api;
